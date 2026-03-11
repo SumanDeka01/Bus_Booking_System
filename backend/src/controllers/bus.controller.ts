eb930cc81ac03c122eba2a18d9ed526d1db137e4
SumanDeka01 <sdeka2903@gmail.com>
2026-03-11 11:13:20 +0530

    Utils added: seatLayout

--- backend/src/controllers/bus.controller.ts
+++ backend/src/controllers/bus.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { Bus } from "../models/bus.model";
+import { generateSeatLayout, getDepartureSlots } from "../utils/seatLayout";
 
 export const getBuses = async (
   req: Request,
@@ -7,12 +8,28 @@ export const getBuses = async (
   next: NextFunction
 ): Promise<void> => {
   try {
-    const buses = await Bus.find();
+    const {
+        departureCity,
+        arrivalCity,
+        date,
+        seatType,
+        isAXC,
+        departureTime
+    }= req.query as Record<string, string>;
+
+    if(!departureCity || !arrivalCity || !date) {
+        res.status(400).json({
+            message: "departureCity, arrivalCity and date are required"
+        });
+        return;
+    }
 
     res.status(200).json({
       message: "Buses fetched successfully",
       buses,
     });
+
+
   } catch (error) {
     next(error);
   }
---
+++ backend/src/utils/seatLayout.ts
@@ -0,0 +1,46 @@
+import {ISeat} from "../models/bus.model";
+
+export const generateSeatLayout = ({
+    totalSeats,
+    seatType,
+    bookedSeats 
+}: {
+    totalSeats: number;
+    seatType: String;
+    bookedSeats?: number
+}): ISeat[] => {
+    const seats: ISeat[] = [];
+
+    for (let i =1; i<= totalSeats; i++) {
+        const row = Math.ceil(i / 4);
+        const column = (i - 1) % 4 + 1;
+        
+
+        seats.push({
+            seatNumber: i,
+            isAvaiable: !bookedSeats?.includes(i),
+            isReserved: false,
+            row,
+            column,
+            seatType,
+            sleeperLevel: 
+                seatType === "sleeper" ? (row <= 2 ? "lower" : "upper") : undefined
+        });
+    }
+    return seats;
+};
+
+//bus slots
+
+export const getDepartureSlots = (timeStr: string): string => {
+    const [time, period] = timeStr.split("");
+    let hour = parseInt(time);
+
+    if (period === "PM" && hour !== 12) hour += 12;
+    if (period === "AM" && hour === 12) hour = 0;
+
+    if (hour >= 0 && hour < 12) return " Morning";
+    if (hour >= 12 && hour < 16) return " Afternoon";
+    if (hour >= 16 && hour < 20) return " Evening";
+    return " Night";
+}
