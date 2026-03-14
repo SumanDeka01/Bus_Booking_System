b3f4c7aac1c4dbcbb7c81664f85204ddeca4fc69
SumanDeka01 <sdeka2903@gmail.com>
2026-03-14 22:02:21 +0530

    setup react app routing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,19 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import BusListPage from "./pages/BusListPage";
+import SeatSelectionPage from "./pages/SeatSelectionPage";
+import BookingConfirmPage from "./pages/BookingConfirmpage";
 
 function App() {
   return (
-    <>
-      <HomePage />
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/buses" element={<BusListPage />} />
+        <Route path="/buses/:busId/seats" element={<SeatSelectionPage />} />
+        <Route path="/booking/confirm" element={<BookingConfirmPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
