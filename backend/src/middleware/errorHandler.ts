a7d6a19ddce479b104859bae87f21a46dc742b63
SumanDeka01 <sdeka2903@gmail.com>
2026-03-14 22:01:35 +0530

    add error handling middleware

---
+++ backend/src/middleware/errorHandler.ts
@@ -0,0 +1,17 @@
+import { Request, Response , NextFunction} from "express";
+
+export const notFound = (req: Request, res: Response, next: NextFunction) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
+    console.error(err);
+
+    const statusCode = err.statusCode || 500;
+
+    res.status(statusCode).json({
+        message: err.message || "server error",
+    })
+};
